43047d9180b7aca715f4a36171600544ef565398
Camila Sola <130872715+camilasolais@users.noreply.github.com>
2023-05-24 09:39:51 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,27 +1,27 @@
-/* --pocicion inicial */
+import { getAnimeQuotes } from './api.js';
+
 let ubicacionPrincipal = window.pageYOffset;
 let $div = document.querySelector("div");
 
-/* --evento scroll */
 window.addEventListener("scroll", function () {
-    /* --muestra la ubicacion cada vez que hagas scroll */
-    //console.log(window.pageYOffset);
+  let desplazamientoActual = window.pageYOffset;
 
-    let desplazamientoActual = window.pageYOffset;
+  if (ubicacionPrincipal >= desplazamientoActual) {
+    $div.style.top = "0px";
+    console.log('Ubicacion Principal');
+    console.log(ubicacionPrincipal);
+    console.log('Desplazamiento');
+    console.log(desplazamientoActual);
+  } else {
+    $div.style.top = "-80px";
+    console.log('Ubicacion Principal');
+    console.log(ubicacionPrincipal);
+    console.log('Desplazamiento');
+    console.log(desplazamientoActual);
+  }
 
-    if (ubicacionPrincipal >= desplazamientoActual) {
-        $div.style.top = "0px";
-        console.log('Ubicacion Principal')
-        console.log(ubicacionPrincipal)
-        console.log('desplazamiento')
-        console.log(desplazamientoActual)
-    } else {
-        $div.style.top = "-80px";
-        console.log('Ubicacion Principal')
-        console.log(ubicacionPrincipal)
-        console.log('desplazamiento')
-        console.log(desplazamientoActual)
-    }
+  ubicacionPrincipal = desplazamientoActual;
 
-    ubicacionPrincipal = desplazamientoActual;
+  // Llama a la función getAnimeQuotes() del archivo api.js
+  getAnimeQuotes();
 });
